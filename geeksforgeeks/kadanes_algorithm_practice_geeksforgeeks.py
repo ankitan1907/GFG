11eda988e4ec59682d2ed16d07ab4857a26f5bfe
Ankita N <165762358+ankitan1907@users.noreply.github.com>
2025-08-28 12:19:21 +0530

    Delete geeksforgeeks directory

--- geeksforgeeks/kadanes_algorithm_practice_geeksforgeeks.py
+++
@@ -1,2 +0,0 @@
-class Solution:
-    def maxSubArraySum(self, arr):
--- geeksforgeeks/solution-2025-08-25T16-12-32.py
+++
@@ -1,5 +0,0 @@
-// Source: https://www.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1?page=1&sortBy=submissions
-// Language: python
-// Generated: 2025-08-25T16:12:32.383Z
-
-class Solution:     def subarraySum(self, arr, target):
