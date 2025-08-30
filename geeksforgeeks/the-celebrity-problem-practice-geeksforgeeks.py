1d2045752035176ddb84dcb1ac5abbb62e5bf60a
Ankita N <165762358+ankitan1907@users.noreply.github.com>
2025-08-30 10:17:11 +0530

    Add python code snippet from The Celebrity Problem | Practice | GeeksforGeeks

---
+++ geeksforgeeks/the-celebrity-problem-practice-geeksforgeeks.py
@@ -0,0 +1,25 @@
+class Solution {
+    public int celebrity(int mat[][]) {
+        int n = mat.length;
+        if(n==1) return 0;
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(mat[i][j]==1)
+                {
+                    arr[j]++;
+                    arr[i]--;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i] == n-1) return i;
+        }
+        return -1;
+        // code here
+        
+    }
+}
