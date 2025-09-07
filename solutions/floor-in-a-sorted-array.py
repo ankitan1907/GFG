50b93cf8d25039a6740484b9502d1d72024af335
Ankita N <165762358+ankitan1907@users.noreply.github.com>
2025-09-07 13:33:18 +0530

    Add code snippet from solution

---
+++ solutions/floor-in-a-sorted-array.py
@@ -0,0 +1,26 @@
+
+class Solution {
+
+    static int findFloor(int[] arr, int x) {
+    
+        
+      
+        int low=0, high = arr.length -1;
+        int result = -1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(arr[mid]<=x){
+                result=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return result;
+    }
+}
+
+   
+
+
